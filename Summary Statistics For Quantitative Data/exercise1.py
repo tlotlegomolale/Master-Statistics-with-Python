bc692f167180a646cdef3d244325f6e386615fdd
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-02-05 15:27:14 +0200

    Update exercise1.py

--- Summary Statistics For Quantitative Data/exercise1.py
+++ Summary Statistics For Quantitative Data/exercise1.py
@@ -1,1 +1,9 @@
-#placeholder
+# Set total equal to the sum
+total = 163
+
+# Set mean_value equal to the mean
+mean_value = 163/4
+
+# The following code prints the total and mean
+print("The sum total is equal to: " + str(total))
+print("The mean value is equal to: " + str(mean_value))
