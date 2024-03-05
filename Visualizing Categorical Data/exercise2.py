62610b5f1465d80c4d7e5c915ee2065e0853ec3f
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-05 14:10:10 +0200

    Update exercise2.py

--- Visualizing Categorical Data/exercise2.py
+++ Visualizing Categorical Data/exercise2.py
@@ -1,1 +1,9 @@
-print('Hello World')
+import codecademylib3
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("games.csv")
+print(df.head())
+
+sns.countplot(df["victory_status"])
