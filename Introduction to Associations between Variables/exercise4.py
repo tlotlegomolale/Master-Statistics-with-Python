a94eaadc42a6436a98ec00b7a482e81f639646e0
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-07 14:49:00 +0200

    Create exercise4.py

---
+++ Introduction to Associations between Variables/exercise4.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import codecademylib3
+import matplotlib.pyplot as plt 
+students = pd.read_csv('students.csv')
+
+scores_urban = students.G3[students.address == 'U']
+scores_rural = students.G3[students.address == 'R']
+
+#create the overlapping histograms here:
+plt.hist(scores_urban, label="Urban", normed=True, alpha=0.5)
+plt.hist(scores_rural, label="Rural", normed=True, alpha=0.5)
+
+
+
+plt.show()
