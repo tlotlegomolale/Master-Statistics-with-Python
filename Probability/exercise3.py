1dcccf8b6ddb4804d5db73428094ea64f6a3eec0
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-16 21:16:42 +0200

    Update exercise3.py

--- Probability/exercise3.py
+++ Probability/exercise3.py
@@ -1,2 +1,11 @@
 #placeholder
 print("Hello World")
+
+
+Name = "Timothy"
+Age = int(input("How old are you {0}".format(Name))
+
+if Age > 18:
+  print("Your are eligibile to Vote")
+          else:
+print("Unlucky")
