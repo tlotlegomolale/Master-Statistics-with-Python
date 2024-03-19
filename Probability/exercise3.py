e4a27a457c0e38e66fb170ef41b0e62b41925f6d
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-19 08:22:20 +0200

    Update exercise3.py

--- Probability/exercise3.py
+++ Probability/exercise3.py
@@ -1,11 +1,19 @@
-#placeholder
-print("Hello World")
+import numpy as np
+
+# create 6 sided "die"
+die_6 = range(1, 7)
+
+# set number of rolls
+num_rolls = 10
+
+# roll the "die" the set amount of times
+results_1 = np.random.choice(die_6, size = num_rolls, replace = True)
+print(results_1)
+
+# create 12-sided "die"
+die_12 = range(1, 13)
 
 
-Name = "Timothy"
-Age = int(input("How old are you {0}".format(Name))
-
-if Age > 18:
-  print("Your are eligibile to Vote")
-          else:
-print("Unlucky")
+# roll the 12-sided "die" 10 times
+#results_2 = 
+results_2 = np.random.choice(die_12, size = 10, replace = True)
