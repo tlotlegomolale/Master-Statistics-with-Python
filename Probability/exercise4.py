f8dc86220408a83369fc9087595e73bf16c71178
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-19 10:34:25 +0200

    Update exercise4.py

--- Probability/exercise4.py
+++ Probability/exercise4.py
@@ -1,4 +1,16 @@
-#placeholder
-print("placeholder")
-print("Oksalayo")
-print("Hade")
+import scipy.stats as stats
+
+# value of interest
+# change this
+x = 3
+
+# sample size
+# change this
+n = 10
+
+# calculate probability
+prob_1 = stats.binom.pmf(x, n, 0.5)
+print(prob_1)
+
+## Question 2
+prob_2 = stats.binom.pmf(7,20,0.5)
