09b64e34cdb74a0d044c22ad7485eca6cf33f485
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-21 00:34:31 +0200

    Update exercise10.py

--- Probability/exercise10.py
+++ Probability/exercise10.py
@@ -1,1 +1,15 @@
-#hello
+import scipy.stats as stats
+
+## Checkpoint 1
+# calculate prob_more_than_20
+prob_more_than_20 = 1 - stats.poisson.cdf(20, 15)
+
+# print prob_more_than_20
+print(prob_more_than_20)
+
+## Checkpoint 
+# calculate prob_17_to_21
+prob_17_to_21 = stats.poisson.cdf(21, 15) - stats.poisson.cdf(16, 15)
+
+# print prob_17_to_21
+print(prob_17_to_21)
