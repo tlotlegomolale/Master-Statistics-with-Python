f45a743c6585dfab8aba88c47b6bb63b5a997950
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-20 09:21:43 +0200

    Update exercise5.py

--- Probability/exercise5.py
+++ Probability/exercise5.py
@@ -1,2 +1,10 @@
-#placeholder
-print("broer")
+import scipy.stats as stats
+
+## Checkpoint 1
+prob_1 = stats.binom.pmf(4, n=10,p=0.5) + stats.binom.pmf(5,n=10, p=0.5) + stats.binom.pmf(6, n=10, p=0.5)
+print(prob_1)
+
+## Checkpoint 2
+prob_2 = 1 - (stats.binom.pmf(1, n=10, p=0.5) + stats.binom.pmf(2, n=10, p=0.5) + stats.binom.pmf(0, n=10, p=0.5))
+print(prob_2)
+
