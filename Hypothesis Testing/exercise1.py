e53f33eff5e2c55af08550827969cced6b8bfb7c
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-03-22 10:14:27 +0200

    Update exercise1.py

--- Hypothesis Testing/exercise1.py
+++ Hypothesis Testing/exercise1.py
@@ -1,1 +1,9 @@
-#hello
+import numpy as np
+
+prices = np.genfromtxt("prices.csv")
+
+# print prices:
+print(prices)
+# calculate prices_mean and print it out:
+prices_mean = np.mean(prices)
+print(prices_mean)
