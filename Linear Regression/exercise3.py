49bb722b94afb1620ce210f096520c5c111a2f3a
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-02 11:52:05 +0200

    Create exercise3.py

---
+++ Linear Regression/exercise3.py
@@ -0,0 +1,24 @@
+# Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# Read in the data
+students = pd.read_csv('test_data.csv')
+
+# Fit the model
+model = sm.OLS.from_formula('score ~ hours_studied', students)
+results = model.fit()
+
+# Print the model params
+print(results.params)
+
+# Calculate and print `pred_3hr` here:
+pred_3hr = results.params[1]*3 + results.params[0]
+print(pred_3hr)
+
+# Calculate and print `pred_5hr` here:
+newdata = {"hours_studied":[5]}
+pred_5hr = results.predict(newdata)
+print(pred_5hr)
