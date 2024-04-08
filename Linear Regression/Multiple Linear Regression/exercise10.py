f173de2e20378cf70a652214716050a2c9f6157a
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-08 23:31:49 +0200

    Create exercise10.py

---
+++ Linear Regression/Multiple Linear Regression/exercise10.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import statsmodels.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import codecademylib3
+
+plants = pd.read_csv('plants.csv')
+model = sm.OLS.from_formula('growth ~ water + fertilizer + water:fertilizer', data=plants).fit()
+
+# Create scatter plot here:
+sns.lmplot(x='water', y='growth', hue='fertilizer', palette='Purples', fit_reg=False, data=plants)
+# Uncomment regression lines here:
+plt.plot(plants.water, model.params[0] + model.params[1]*plants.water+ model.params[2]*2 + model.params[3]*plants.water*2, color='lavender', linewidth=3)
+plt.plot(plants.water, model.params[0] + model.params[1]*plants.water+ model.params[2]*4 + model.params[3]*plants.water*4, color='mediumpurple', linewidth=3, label='fertilizer=4')
+# Add third line here:
+plt.plot(plants.water, model.params[0] + model.params[1]*plants.water+ model.params[2]*6 + model.params[3]*plants.water*6, color='indigo', linewidth=3, label='fertilizer=6')
+# Uncomment legend and display code here:
+plt.legend(['fertilizer=2','fertilizer=4','fertilizer=6'])
+plt.show()
