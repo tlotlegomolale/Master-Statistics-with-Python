0a3eefa592839c64f8c309c320ddaa9d7493ad4c
tlotlegomolale <144572551+tlotlegomolale@users.noreply.github.com>
2024-04-08 15:01:57 +0200

    Update exercise1.py

--- Linear Regression/Multiple Linear Regression/exercise1.py
+++ Linear Regression/Multiple Linear Regression/exercise1.py
@@ -1,1 +1,10 @@
-#placeholder
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import codecademylib3
+
+student = pd.read_csv('student.csv')
+
+# Add code for scatter plot here:
+sns.lmplot(x='math1', y='port3', hue='address', markers=['o', 'x'],fit_reg=False, data=student)
+plt.show()
